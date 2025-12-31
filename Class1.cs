dac0665f5c6f3e16b7001818eb4c856f54ec1bfa
TMYX <78tmyx.ftr@gmail.com>
2025-12-31 16:25:30 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+public class BuildAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        UnityEngine.Debug.Log("AssetBundles built!");
+    }
+}
