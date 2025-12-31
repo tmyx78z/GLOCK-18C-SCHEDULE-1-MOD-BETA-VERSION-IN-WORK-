ff021a0a380a18a501bebc61752642094d63904f
TMYX <78tmyx.ftr@gmail.com>
2025-12-31 16:25:06 +0100

    Add files via upload

---
+++ glock/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildBundlesEditor
+{
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class BuildBundlesEditor
+{
+    [MenuItem("Assets/Build All AssetBundles")]
+    static void BuildAllBundles()
+    {
+        string outputPath = "Assets/AssetBundles";
+
+        // Crée le dossier si il n'existe pas
+        if (!Directory.Exists(outputPath))
+            Directory.CreateDirectory(outputPath);
+
+        // Construire tous les AssetBundles
+        BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+
+        Debug.Log("AssetBundles built in " + outputPath);
+    }
+}
---
+++ glock/glock18C.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+public class BuildBundlesPlay : MonoBehaviour
+{
+    void Start()
+    {
+        string outputPath = "Assets/AssetBundles";
+
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+
+        BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+
+        Debug.Log("AssetBundles built in " + outputPath);
+    }
+}
